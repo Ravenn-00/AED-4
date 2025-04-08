617ca165a70668e83c3b0f26c01fc791853e58fb
GRB <freze000@gmail.com>
2025-04-08 16:21:09 -0500

    Update Subconjunto.java

--- Subconjunto.java
+++ Subconjunto.java
@@ -1,3 +1,11 @@
 public class Subconjunto {
+    public static void comprobar(int[] arr) {
+        int[] subArray = new int[arr[0]];
+        int suma = arr[arr.length - 1];
+        for(int i = 1; i < arr.length - 1; i++){
+            
+
+        }
+    }
     
 }
